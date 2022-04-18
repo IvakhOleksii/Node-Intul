7cad38d25cbebdb73432925e1fc4ebffde351216
ivakholeksii <oleksiiivakh@gmail.com>
2022-04-18 03:04:22 -0500

    Add InsertQuery BigQuery service method

--- src/services/BigQueryService.ts
+++ src/services/BigQueryService.ts
@@ -1,5 +1,6 @@
 import { BigQuery } from '@google-cloud/bigquery';
 import { config } from 'dotenv';
+import { Tables } from '../types/Common';
 import { User } from '../types/User';
 import { consoleLog } from '../utils';
 
@@ -42,5 +43,33 @@ export class BigQueryService {
             console.log(error);
         }
         return null;
+    }
+
+    public static async insertQuery(dataset: string, table: string, data: any) {
+        try {
+            consoleLog(`BIGQUERY: INSERT QUERY = ${JSON.stringify(data)}`);
+            const keys = Object.keys(data)
+            const values = keys.map(k => `"""${data[k]}"""`);
+          
+            const query = `
+              INSERT INTO \`${dataset}.${table}\`
+              (${keys.join(', ')})
+              VALUES (${values.join(', ')})
+            `;
+            console.log(query)
+
+            const options = {
+                query,
+                location: 'US',
+            };
+
+            const [job] = await BigQueryService.getClient().createQueryJob(options);
+            const res = await job.getQueryResults();
+
+            return { result: true, res};
+        } catch (error) {
+            console.log(error);
+            return { result: false, error  };
+        }
     }
 }
