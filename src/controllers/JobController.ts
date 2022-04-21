5adaddf8ec2c57c1a937aceceab10ed1387e0522
ivakholeksii <oleksiiivakh@gmail.com>
2022-04-21 00:20:02 -0500

    Add ability to have advanced filtering with AND or OR conditions. Implement with Jobs /search

--- src/controllers/JobController.ts
+++ src/controllers/JobController.ts
@@ -1,120 +1,188 @@
-import { Controller, Param, Body, Get, Post, Put, Delete, QueryParam, JsonController, Authorized, CurrentUser, Patch } from 'routing-controllers';
-import { BigQueryService } from '../services/BigQueryService';
-import { GetroService } from '../services/GetroService';
-import { createJob, getAppliedJobsByUser, updateJob } from '../services/Job';
-import { DATASET_BULLHORN, DATASET_GETRO, FilterOption, Job, FilterBody, JobSearchByFilterResponse, JobSearchByID, DataSource, Tables, ApplyResponse, GetSavedJobsResponse } from '../types/Common';
-import { User } from '../types/User';
-import { getDataSource } from '../utils';
-import { JobFilter } from '../utils/FieldMatch';
-import { getCandidatesOnJob, getSavedJobs, saveApplication, saveJob } from '../utils/MainCrud';
+import {
+  Controller,
+  Param,
+  Body,
+  Get,
+  Post,
+  Put,
+  Delete,
+  QueryParam,
+  JsonController,
+  Authorized,
+  CurrentUser,
+  Patch,
+} from "routing-controllers";
+import { BigQueryService } from "../services/BigQueryService";
+import { GetroService } from "../services/GetroService";
+import { createJob, getAppliedJobsByUser, updateJob } from "../services/Job";
+import {
+  DATASET_BULLHORN,
+  DATASET_GETRO,
+  FilterOption,
+  Job,
+  FilterBody,
+  JobSearchByFilterResponse,
+  JobSearchByID,
+  DataSource,
+  Tables,
+  ApplyResponse,
+  GetSavedJobsResponse,
+  AdvancedFilterOption,
+} from "../types/Common";
+import { User } from "../types/User";
+import { getDataSource } from "../utils";
+import { JobFilter } from "../utils/FieldMatch";
+import {
+  getCandidatesOnJob,
+  getSavedJobs,
+  saveApplication,
+  saveJob,
+} from "../utils/MainCrud";
 
-@JsonController('/api/job')
+@JsonController("/api/job")
 export class JobController {
-  @Get('/search')
-  async searchJobByID(
-    @QueryParam('id') id: string
-  ): Promise<JobSearchByID> {
+  @Get("/search")
+  async searchJobByID(@QueryParam("id") id: string): Promise<JobSearchByID> {
     const datasource = getDataSource(id);
     if (datasource === DataSource.BULLHORN) {
       const job = await this.getJobFromBullhorn(id);
       return {
         job,
-        source: datasource
+        source: datasource,
       };
-    }
-    else if(datasource === DataSource.GETRO) {
+    } else if (datasource === DataSource.GETRO) {
       const job = await this.getJobFromGetro(id);
       return {
         job,
-        source: datasource
+        source: datasource,
       };
-    }
-    else {
+    } else {
       return {
         source: DataSource.UNKNOWN,
-        message: 'unsupported id'
+        message: "unsupported id",
       };
     }
   }
 
   async getJobFromBullhorn(id: string): Promise<Job | undefined> {
-    const fields = '*';
+    const fields = "*";
     const dataset = DATASET_BULLHORN;
     const table = Tables.JOBS;
     const condition = `id = '${id}'`;
-    const result = (await BigQueryService.selectQuery(dataset, table, fields, undefined, condition)) as Job[]; 
+    const result = (await BigQueryService.selectQuery(
+      dataset,
+      table,
+      fields,
+      undefined,
+      condition
+    )) as Job[];
     return result ? result[0] : undefined;
   }
 
   async getJobFromGetro(id: string): Promise<Job | undefined> {
-    const fields = '*';
+    const fields = "*";
     const dataset = DATASET_GETRO;
     const table = Tables.JOBS;
     const condition = `id = '${id}'`;
-    const result = (await BigQueryService.selectQuery(dataset, table, fields, undefined, condition)) as Job[]; 
+    const result = (await BigQueryService.selectQuery(
+      dataset,
+      table,
+      fields,
+      undefined,
+      condition
+    )) as Job[];
     return result ? result[0] : undefined;
   }
 
-  @Post('/search')
+  @Post("/search")
   async searchByFilter(
     @Body() body: FilterBody
   ): Promise<JobSearchByFilterResponse> {
     try {
       const { filters, fields, page, count } = body;
-      const bullhornJobs = (await this.getJobsByFilterFromBullhorn(filters, fields, page, count)) as Job[] ;
+      const bullhornJobs = (await this.getJobsByFilterFromBullhorn(
+        filters,
+        fields,
+        page,
+        count
+      )) as Job[];
       const response = {
         jobs: bullhornJobs,
         total: bullhornJobs?.length,
-        message: null
+        message: null,
       };
       return response;
     } catch (error) {
       return {
-        message: error
+        message: error,
       };
     }
   }
 
   async getJobsByFilterFromBullhorn(
-    filters: FilterOption[] | undefined,
+    filters: AdvancedFilterOption[] | undefined,
     fields: string[] | null,
     page: number,
     count: number
   ) {
-    if (filters?.map(opt => Object.keys(JobFilter.bullhorn).indexOf(opt.key) > -1).every(val => val === true)) {
-      const _fields = fields ? fields.join(', ') : '*';
+    if (
+      filters
+        ?.map((opt) => Object.keys(JobFilter.bullhorn).indexOf(opt.key) > -1)
+        .every((val) => val === true)
+    ) {
+      const _fields = fields ? fields.join(", ") : "*";
       const _dataset = DATASET_BULLHORN;
       const _table = Tables.JOBS;
-      const _condition = filters && filters
-        .map(opt => `LOWER(${JobFilter.bullhorn[opt.key]}) LIKE '%${opt.value.toLowerCase()}%'`)
-        .join(' AND ');
-      return await BigQueryService.selectQuery(_dataset, _table, _fields, count, _condition);
+      const _condition =
+        filters &&
+        filters
+          .map((opt) => {
+            // TODO: Cleanup and move this out to a util function
+            if (Array.isArray(opt.value)) {
+              const operator = opt.operator || "OR";
+              const baseCondition = (value: string) =>
+                `LOWER(${
+                  JobFilter.bullhorn[opt.key]
+                }) LIKE '%${value.toLowerCase()}%'`;
+              return opt.value.map(baseCondition).join(` ${operator} `);
+            } else {
+              return `LOWER(${
+                JobFilter.bullhorn[opt.key]
+              }) LIKE '%${opt.value.toLowerCase()}%'`;
+            }
+          })
+          .join(" AND ");
+      return await BigQueryService.selectQuery(
+        _dataset,
+        _table,
+        _fields,
+        count,
+        _condition
+      );
     }
     throw "invalid filter options";
   }
 
   @Authorized()
-  @Get('/apply')
+  @Get("/apply")
   async apply(
-    @QueryParam('candidate') candidate: string,
-    @QueryParam('job') job: string
+    @QueryParam("candidate") candidate: string,
+    @QueryParam("job") job: string
   ): Promise<ApplyResponse> {
     return await saveApplication(job, candidate);
   }
 
   @Authorized()
-  @Get('/candidates')
-  async getCandidates(
-    @QueryParam('job') job: string
-  ) {
+  @Get("/candidates")
+  async getCandidates(@QueryParam("job") job: string) {
     return await getCandidatesOnJob(job);
   }
 
   @Authorized()
-  @Put('/save')
+  @Put("/save")
   async save(
-    @QueryParam('candidate') candidate: string,
-    @QueryParam('job') job: string
+    @QueryParam("candidate") candidate: string,
+    @QueryParam("job") job: string
   ): Promise<ApplyResponse> {
     return await saveJob(job, candidate);
   }
@@ -124,31 +192,29 @@ export class JobController {
   async update(
     @Body() body: Partial<Job> & { id: string }
   ): Promise<ApplyResponse> {
-    return await updateJob(body)
+    return await updateJob(body);
   }
 
   @Authorized()
   @Get("/applied")
   async getAppliedJobs(
     @CurrentUser() authUser: User,
-    @QueryParam('candidate') candidate: string,
+    @QueryParam("candidate") candidate: string
   ) {
     return await getAppliedJobsByUser(candidate || authUser.id!);
   }
 
   @Authorized()
-  @Get('/savedJobs')
+  @Get("/savedJobs")
   async savedJobs(
-    @QueryParam('candidate') candidate: string,
+    @QueryParam("candidate") candidate: string
   ): Promise<GetSavedJobsResponse> {
     return await getSavedJobs(candidate);
   }
 
   @Authorized()
   @Put()
-  async createJob(
-    @Body() body: Job
-  ): Promise<ApplyResponse> {
-    return await createJob(body)
+  async createJob(@Body() body: Job): Promise<ApplyResponse> {
+    return await createJob(body);
   }
 }
